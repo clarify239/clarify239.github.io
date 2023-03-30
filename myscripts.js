a1031b453123a1e85c40040783294201110e9545
clarify239 <claireqcheng@gmail.com>
2023-03-30 14:36:03 -0400

    hack effect + link to projects

--- myscripts.js
+++ myscripts.js
@@ -28,3 +28,32 @@ function showSlide(n) {
   slides[slideIndex].style.display = "block";
   captions[slideIndex].style.display = "block";
 }
+
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".Claire_Cheng").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    iteration ++;
+  }, 40);
+}
